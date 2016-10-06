b932e36c0f0ee1511c6819311d9e65d5171af174
dswatson <david.s.watson11@gmail.com>
2016-10-06 11:29:07 +0100

    lower case

--- R/plot_pca.R
+++ R/plot_pca.R
@@ -1,40 +1,41 @@
 #' Plot the first two principal components of an omic data matrix
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns 
-#'   to samples.
-#' @param group Optional factor or character vector of length equal to sample size. 
+#' @param dat Omic data matrix with rows corresponding to probes and columns
+#'   to samples. It is strongly recommended that data be filtered and
+#'   normalized prior to running PCA.
+#' @param group Optional factor or character vector of length equal to sample size.
 #'   Levels are used to color and shape points.
-#' @param label Label data points by sample name? Defaults to FALSE unless 
+#' @param label Label data points by sample name? Defaults to FALSE unless
 #'   \code{group = NULL}.
-#' @param main Optional plot title. 
-#' @param legend Legend position. Must be one of \code{"outside", 
+#' @param main Optional plot title.
+#' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
 #'
-#' @details 
-#' This function plots the first two principal components of an omic data 
-#' matrix. The numbers printed in the x- and y-axis labels indicate the 
-#' percentage of variance explained by each component. PCA is an easy and 
-#' popular method for unsupervised cluster detection. It can also aid in 
-#' spotting potential outliers and generally  help to visualize the latent 
+#' @details
+#' This function plots the first two principal components of an omic data
+#' matrix. The numbers printed in the x- and y-axis labels indicate the
+#' percentage of variance explained by each component. PCA is an easy and
+#' popular method for unsupervised cluster detection. It can also aid in
+#' spotting potential outliers and generally  help to visualize the latent
 #' structure of a data set.
-#' 
+#'
 #' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_pca(mat)
-#' 
+#'
 #' library(DESeq2)
 #' mat <- cbind(matrix(rnbinom(5000, mu = 4, size = 1), nrow = 1000, ncol = 5),
 #'              matrix(rnbinom(5000, mu = 4, size = 10), nrow = 1000, ncol = 5))
 #' mat <- rlog(mat)
-#' batch <- rep(c("A", "B"), each = 5)
-#' plot_pca(mat, group = batch)
+#' grp <- rep(c("A", "B"), each = 5)
+#' plot_pca(mat, group = grp)
 #'
 #' @export
 #' @import dplyr
 #' @import ggplot2
 #'
 
-plot_PCA <- function(dat, 
+plot_pca <- function(dat,
                      group  = NULL,
                      label  = FALSE,
                      main   = NULL,
@@ -57,20 +58,20 @@ plot_PCA <- function(dat,
     main <- 'PCA'
   }
 
-  pca <- prcomp(t(dat), center = TRUE, scale. = TRUE) 
+  pca <- prcomp(t(dat), center = TRUE, scale. = TRUE)
   var1 <- round(pca$sdev[1]^2 / sum(pca$sdev^2) * 100, 2)
   var2 <- round(pca$sdev[2]^2 / sum(pca$sdev^2) * 100, 2)
-  df <- data_frame(PC1    = pca$x[, 1], 
+  df <- data_frame(PC1    = pca$x[, 1],
                    PC2    = pca$x[, 2],
                    Sample = sample,
                    Group  = group)
-  
-  p <- ggplot(df, aes(PC1, PC2)) + 
-    geom_hline(yintercept = 0, size = 0.2) + 
+
+  p <- ggplot(df, aes(PC1, PC2)) +
+    geom_hline(yintercept = 0, size = 0.2) +
     geom_vline(xintercept = 0, size = 0.2) +
-    labs(title = main, 
-         x = paste0('PC1 (', var1, '%)'), 
-         y = paste0('PC2 (', var2, '%)')) + 
+    labs(title = main,
+         x = paste0('PC1 (', var1, '%)'),
+         y = paste0('PC2 (', var2, '%)')) +
     theme_bw()
 
   if (label == TRUE) {
