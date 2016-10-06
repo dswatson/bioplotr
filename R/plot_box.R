77ac25908af4fcda0a9446d9460b9303ae692a10
dswatson <david.s.watson11@gmail.com>
2016-10-06 11:28:53 +0100

    EDA fix

--- R/plot_box.R
+++ R/plot_box.R
@@ -1,29 +1,29 @@
 #' Create box plots by sample
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns 
+#' @param dat Omic data matrix with rows corresponding to probes and columns
 #'   to samples.
-#' @param group Optional factor or character vector of length equal to sample size. 
+#' @param group Optional factor or character vector of length equal to sample size.
 #'   Levels are used to color box plots.
-#' @param type Optional string specifying omic data type. Currently supports 
+#' @param type Optional string specifying omic data type. Currently supports
 #'   \code{"microarray", "RNA-seq",} and \code{"methylation"}.
-#' @param ylab Label for y-axis. If left \code{NULL}, this defaults to 
-#'   \eqn{log_2} expression for \code{type = "microarray"}, \eqn{log_2} CPM for
-#'   \code{type = "RNA-seq"}, and Beta for \code{type = "methylation"}. At least 
+#' @param ylab Label for y-axis. If left \code{NULL}, this defaults to
+#'   log expression for \code{type = "microarray"}, log CPM for
+#'   \code{type = "RNA-seq"}, and Beta for \code{type = "methylation"}. At least
 #'   one of \code{type} or \code{xlab} must be specified.
-#' @param main Optional plot title. 
-#' @param legend Legend position. Must be one of \code{"outside", 
+#' @param main Optional plot title.
+#' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
 #'
-#' @details 
-#' This function displays each sample's distribution of expression or methylation 
-#' values in a single plot. It is especially helpful when contrasting pre- and 
-#' post-normalization matrices. It may additionally be used to inspect for batch effects 
+#' @details
+#' This function displays each sample's distribution of expression or methylation
+#' values in a single plot. It is especially helpful when contrasting pre- and
+#' post-normalization matrices. It may additionally be used to inspect for batch effects
 #' or other associations with phenotypic categories using the \code{group} argument.
-#' 
+#'
 #' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_box(mat, type = "microarray")
-#' 
+#'
 #' library(edgeR)
 #' mat <- cbind(matrix(rnbinom(5000, mu = 4, size = 1), nrow = 1000, ncol = 5),
 #'              matrix(rnbinom(5000, mu = 4, size = 10), nrow = 1000, ncol = 5))
@@ -33,16 +33,16 @@
 #' plot_box(mat, group = batch, type = "RNA-seq")
 #'
 #' @export
-#' @importFrom tidyr gather 
+#' @importFrom tidyr gather
 #' @import dplyr
 #' @import ggplot2
 #'
 
-plot_box <- function(dat, 
-                     group  = NULL, 
+plot_box <- function(dat,
+                     group  = NULL,
                      type   = NULL,
-                     ylab   = NULL, 
-                     main   = NULL, 
+                     ylab   = NULL,
+                     main   = NULL,
                      legend = 'outside') {
 
   df <- gather(as_tibble(dat), Sample, Expression) %>%
@@ -50,7 +50,7 @@ plot_box <- function(dat,
 
   if (!is.null(group)) {
     df <- mutate(df, Group = rep(group, each = nrow(dat)))
-  } 
+  }
   if (is.null(main) & is.null(group)) {
     main <- 'Expression By Sample'
   }
@@ -62,7 +62,7 @@ plot_box <- function(dat,
   }
   if (type == 'microarray') {
     ylab <- expression('log'[2]*' Expression')
-  } 
+  }
   if (type == 'RNA-seq') {
     ylab <- expression('log'[2]*' Counts Per Million')
   }
@@ -70,10 +70,10 @@ plot_box <- function(dat,
     ylab <- 'Beta'
   }
 
-  p <- ggplot(df, aes(Sample, Expression)) + 
-    labs(title = main, y = ylab) + 
-    theme_bw() + 
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+  p <- ggplot(df, aes(Sample, Expression)) +
+    labs(title = main, y = ylab) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   if (!is.null(group)) {
     p <- p + geom_boxplot(aes(fill = Group))
--- R/plot_density.R
+++ R/plot_density.R
@@ -1,54 +1,54 @@
 #' Create density plots by sample
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns 
+#' @param dat Omic data matrix with rows corresponding to probes and columns
 #'   to samples.
-#' @param group Optional factor or character vector of length equal to sample size. 
+#' @param group Optional factor or character vector of length equal to sample size.
 #'   Levels are used to color density curves.
-#' @param type Optional string specifying omic data type. Currently supports 
+#' @param type Optional string specifying omic data type. Currently supports
 #'   \code{"microarray", "RNA-seq",} or \code{"methylation"}.
-#' @param xlab Label for x-axis. If left \code{NULL}, this defaults to 
-#'   \eqn{log_2} expression for \code{type = "microarray"}, \eqn{log_2} CPM for
-#'   \code{type = "RNA-seq"}, and Beta for \code{type = "methylation"}. At least 
+#' @param xlab Label for x-axis. If left \code{NULL}, this defaults to
+#'   log expression for \code{type = "microarray"}, log CPM for
+#'   \code{type = "RNA-seq"}, and Beta for \code{type = "methylation"}. At least
 #'   one of \code{type} or \code{xlab} must be specified.
-#' @param main Optional plot title. 
-#' @param legend Legend position. Must be one of \code{"outside", 
+#' @param main Optional plot title.
+#' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
 #'
-#' @details 
-#' This function displays each sample's distribution of expression or methylation 
-#' values in a single plot. It is especially helpful when contrasting pre- and 
-#' post-normalization matrices. It may additionally be used to inspect for batch effects 
+#' @details
+#' This function displays each sample's distribution of expression or methylation
+#' values in a single plot. It is especially helpful when contrasting pre- and
+#' post-normalization matrices. It may additionally be used to inspect for batch effects
 #' or other associations with phenotypic categories by using the \code{group} argument.
-#' 
+#'
 #' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_density(mat, type = "microarray")
-#' 
+#'
 #' library(DESeq2)
 #' mat <- cbind(matrix(rnbinom(5000, mu = 4, size = 1), nrow = 1000, ncol = 5),
 #'              matrix(rnbinom(5000, mu = 4, size = 10), nrow = 1000, ncol = 5))
 #' mat <- rlog(mat)
 #' batch <- rep(c("A", "B"), each = 5)
-#' plot_density(mat, group = batch, type = "RNA-seq", 
+#' plot_density(mat, group = batch, type = "RNA-seq",
 #'              main = "rlog Transformed Counts")
 #'
 #' @export
-#' @importFrom tidyr gather 
+#' @importFrom tidyr gather
 #' @import dplyr
 #' @import ggplot2
 #'
 
-plot_density <- function(dat, 
-                         group  = NULL, 
+plot_density <- function(dat,
+                         group  = NULL,
                          type   = NULL,
                          xlab   = NULL,
-                         main   = NULL, 
+                         main   = NULL,
                          legend = 'outside') {
 
   densities <- gather(as_tibble(dat), Sample, Expression) %>%
     group_by(Sample) %>%
     do(Expression = density(.$Expression)$x,
-       Density    = density(.$Expression)$y) 
+       Density    = density(.$Expression)$y)
   densities <- densities[match(colnames(as_tibble(dat)), densities$Sample), ]
 
   df <- data_frame(Sample     = rep(densities$Sample, each = 512),
@@ -57,10 +57,10 @@ plot_density <- function(dat,
 
   if (!is.null(group)) {
     df <- mutate(df, Group = rep(group, each = 512))
-  } 
+  }
   if (is.null(main) & is.null(group)) {
     main <- 'Density By Sample'
-  }  
+  }
   if (is.null(main) & !is.null(group)) {
     main <- 'Density By Group'
   }
@@ -69,7 +69,7 @@ plot_density <- function(dat,
   }
   if (type == 'microarray') {
     xlab <- expression('log'[2]*' Expression')
-  } 
+  }
   if (type == 'RNA-seq') {
     xlab <- expression('log'[2]*' Counts Per Million')
   }
@@ -77,9 +77,9 @@ plot_density <- function(dat,
     xlab <- 'Beta'
   }
 
-  p <- ggplot(df, aes(Expression, Density, group = Sample)) + 
-    labs(title = main, x = xlab) + 
-    theme_bw() 
+  p <- ggplot(df, aes(Expression, Density, group = Sample)) +
+    labs(title = main, x = xlab) +
+    theme_bw()
 
   if (!is.null(group)) {
     p <- p + geom_path(aes(colour = Group))
