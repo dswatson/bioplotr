e22515b1239ca0be0516dd7ad280f4c5866392ed
dswatson <david.s.watson11@gmail.com>
2016-10-06 17:20:25 +0100

    reshuffle

--- R/plot_sim_mat.R
+++ R/plot_sim_mat.R
@@ -1,21 +1,21 @@
 #' Visualize a similarity matrix
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns 
+#' @param dat Omic data matrix with rows corresponding to probes and columns
 #'   to samples.
-#' @param group Optional factor or character vector of length equal to sample size. 
+#' @param group Optional factor or character vector of length equal to sample size.
 #'   Levels are used to color an annotation track atop the heatmap.
 #' @param main Optional plot title.
-#' 
-#' @details 
+#'
+#' @details
 #' This function displays the pairwise scaled Euclidean distance between samples
 #' in the form of a heatmap. A hierarchical clustering dendrogram is added
 #' atop the figure to help identify potential outliers and/or clusters in
-#' the data. 
+#' the data.
 #'
-#' @examples 
+#' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_sim_mat(mat, main = "Nothin' Doin'")
-#' 
+#'
 #' library(DESeq2)
 #' mat <- cbind(matrix(rnbinom(5000, mu = 4, size = 1), nrow = 1000, ncol = 5),
 #'              matrix(rnbinom(5000, mu = 4, size = 10), nrow = 1000, ncol = 5))
@@ -25,11 +25,11 @@
 #'
 #' @importFrom wordspace dist.matrix
 #' @importFrom NMF aheatmap
-#' @import RColorBrewer 
-#' 
+#' @import RColorBrewer
+#'
 
-plot_sim_mat <- function(dat, 
-	                       group = NULL, 
+plot_sim_mat <- function(dat,
+	                       group = NULL,
 	                       main  = NULL) {
 
   if (is.null(main)) {
@@ -40,12 +40,12 @@ plot_sim_mat <- function(dat,
   rb <- colorRampPalette(brewer.pal(10, 'RdBu'))(n = 256)
 
   if (is.null(group)) {
-    aheatmap(dm, col = rb, Rowv = FALSE, 
-      distfun = function(x) as.dist(x), hclustfun = 'average', 
+    aheatmap(dm, col = rb, Rowv = FALSE,
+      distfun = function(x) as.dist(x), hclustfun = 'average',
       main = main)
   } else {
-    aheatmap(dm, col = rb, Rowv = FALSE, annCol = list(Group = group), 
-      distfun = function(x) as.dist(x), hclustfun = 'average', 
+    aheatmap(dm, col = rb, Rowv = FALSE, annCol = list(Group = group),
+      distfun = function(x) as.dist(x), hclustfun = 'average',
       main = main)
   }
 
