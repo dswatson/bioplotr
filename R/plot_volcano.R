09afb3cd5e5b39e63a03bf545afad079ef66e40b
dswatson <david.s.watson11@gmail.com>
2016-10-06 17:20:14 +0100

    space fix

--- R/plot_volcano.R
+++ R/plot_volcano.R
@@ -1,28 +1,28 @@
 #' Create volcano plot of \eqn{log_2} fold changes against \eqn{-log_10 p}-values
 #'
-#' @param dat Data frame or matrix representing the results of a test for 
-#'   differential expression or methylation, such as the output of a call to 
-#'   limma::topTable, edgeR::topTags, or DESeq2::results. Alternatively, any object 
+#' @param dat Data frame or matrix representing the results of a test for
+#'   differential expression or methylation, such as the output of a call to
+#'   limma::topTable, edgeR::topTags, or DESeq2::results. Alternatively, any object
 #'   with columns for p-values, log fold changes, and FDR.
 #' @param fdr Threshold for declaring a probe differentially expressed/methylated.
-#' @param ptsize Size of data points in the plot. 
-#' @param main Optional plot title. 
-#' @param legend Legend position. Must be one of \code{"outside", 
+#' @param ptsize Size of data points in the plot.
+#' @param main Optional plot title.
+#' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
 #'
-#' @details 
-#' This function displays the results of a differential expression or methylation 
-#' test as a volcano plot, with colors distinguishing probes that meet the 
+#' @details
+#' This function displays the results of a differential expression or methylation
+#' test as a volcano plot, with colors distinguishing probes that meet the
 #' user-defined FDR threshold. These figures can be used to evaluate the symmetry,
-#' magnitude, and significance of fold changes for a given experiment. 
-#' 
+#' magnitude, and significance of fold changes for a given experiment.
+#'
 #' @examples
 #' library(dplyr)
 #' df <- data_frame(logFC   = c(rnorm(50, 0, 10), rnorm(4950)),
 #'                  p.value = pnorm(-abs(logFC)),
 #'                  FDR     = p.adjust(p.value, method = 'fdr'))
 #' plot_volcano(df)
-#' 
+#'
 #' library(limma)
 #' DE_genes <- cbind(matrix(rnorm(250, 5, 1), nrow = 50, ncol = 5),
 #'                   matrix(rnorm(250), nrow = 50, ncol = 5))
@@ -39,13 +39,13 @@
 #' @import ggplot2
 #'
 
-plot_volcano <- function(dat, 
-                         fdr    = 0.05, 
+plot_volcano <- function(dat,
+                         fdr    = 0.05,
                          ptsize = 0.25,
-                         main   = NULL, 
+                         main   = NULL,
                          legend = 'outside') {
 
-  dat <- as_tibble(na.omit(dat))
+  dat <- as_data_frame(na.omit(dat))
   if ('P.Value' %in% colnames(dat)) {
     dat <- dat %>% rename(p.value = P.Value)
   }
@@ -58,8 +58,8 @@ plot_volcano <- function(dat,
   if (!'P.Value' %in% colnames(dat) &
       !'pvalue'  %in% colnames(dat) &
       !'p.value' %in% colnames(dat)) {
-    stop('dat must include a p-value column. Recognized colnames for this 
-  vector include "p.value", "P.Value", "PValue", and "pvalue". Make sure 
+    stop('dat must include a p-value column. Recognized colnames for this
+  vector include "p.value", "P.Value", "PValue", and "pvalue". Make sure
   that dat includes exactly one such colname.')
   }
   if ('adj.P.Val' %in% colnames(dat)) {
@@ -75,8 +75,8 @@ plot_volcano <- function(dat,
       !'FDR' %in% colnames(dat) &
       !'padj' %in% colnames(dat) &
       !'q.value' %in% colnames(dat)) {
-    stop('dat must include a column for adjusted p-values. Recognized colnames 
-  for this vector include "q.value", "adj.P.Val", "FDR", "padj", and "FDR". 
+    stop('dat must include a column for adjusted p-values. Recognized colnames
+  for this vector include "q.value", "adj.P.Val", "FDR", "padj", and "FDR".
   Make sure that dat includes exactly one such colname.')
   }
   if ('log2FoldChange' %in% colnames(dat)) {
@@ -84,8 +84,8 @@ plot_volcano <- function(dat,
   }
   if (!'log2FoldChange' %in% colnames(dat) &
       !'logFC' %in% colnames(dat)) {
-    stop('dat must include a log fold change column. Recognized colnames for 
-  this vector include "logFC" and "log2FoldChange". Make sure that dat includes 
+    stop('dat must include a log fold change column. Recognized colnames for
+  this vector include "logFC" and "log2FoldChange". Make sure that dat includes
   exactly one such colname.')
   }
 
@@ -94,30 +94,30 @@ plot_volcano <- function(dat,
   }
 
   test <- function(q) ifelse(q < fdr, TRUE, FALSE)
-  df <- dat %>% 
-    mutate(is.DE = map_lgl(q.value, test), 
+  df <- dat %>%
+    mutate(is.DE = map_lgl(q.value, test),
            logP  = -log10(p.value)) %>%
     select(logFC, logP, is.DE)
 
   if (sum(df$is.DE == TRUE) == 0) {
-    warning('dat returned no differentially expressed/methylated probes at your 
-  selected fdr threshold. To color points by differential expression/methylation, 
+    warning('dat returned no differentially expressed/methylated probes at your
+  selected fdr threshold. To color points by differential expression/methylation,
   consider raising your fdr cutoff.')
-    p <- ggplot(df, aes(logFC, logP)) + 
+    p <- ggplot(df, aes(logFC, logP)) +
       geom_point(size = ptsize, alpha = 0.25)
   } else {
-    p <- ggplot(df, aes(logFC, logP, color = is.DE)) + 
-      geom_point(size = ptsize, alpha = 0.25) + 
-      scale_colour_manual(name   = expression(italic(q)*'-value'), 
+    p <- ggplot(df, aes(logFC, logP, color = is.DE)) +
+      geom_point(size = ptsize, alpha = 0.25) +
+      scale_colour_manual(name   = expression(italic(q)*'-value'),
                           labels = c(paste('\u2265', fdr), paste('<', fdr)),
-                          values = c('black', 'red')) + 
+                          values = c('black', 'red')) +
       guides(col = guide_legend(reverse = TRUE))
   }
 
-  p <- p + labs(title = main, 
-    x = expression('log'[2]*' Fold Change'), 
-    y = expression('-log'[10]*' '*italic(p))) + 
-    theme_bw()  
+  p <- p + labs(title = main,
+    x = expression('log'[2]*' Fold Change'),
+    y = expression('-log'[10]*' '*italic(p))) +
+    theme_bw()
 
   if (legend == 'bottomleft') {
     p <- p + theme(legend.justification = c(0, 0), legend.position = c(0, 0))
