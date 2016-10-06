7576f2eb47576d095cafaaa6a22f3dc0c134dfd3
dswatson <david.s.watson11@gmail.com>
2016-10-06 17:20:41 +0100

    message fix

--- R/plot_md.R
+++ R/plot_md.R
@@ -1,21 +1,21 @@
 #' Create MD plot of \eqn{log_2} fold changes against mean expression or methylation
 #'
-#' @param dat Data frame or matrix representing the results of a test for 
-#'   differential expression or methylation, such as the output of a call to 
-#'   limma::topTable, edgeR::topTags, or DESeq2::results. Alternatively, any 
+#' @param dat Data frame or matrix representing the results of a test for
+#'   differential expression or methylation, such as the output of a call to
+#'   limma::topTable, edgeR::topTags, or DESeq2::results. Alternatively, any
 #'   object with columns for p-values, log fold changes, and FDR.
 #' @param fdr Threshold for declaring a probe differentially expressed or methylated.
-#' @param ptsize Size of data points in the plot. 
-#' @param main Optional plot title. 
-#' @param legend Legend position. Must be one of \code{"outside", 
+#' @param ptsize Size of data points in the plot.
+#' @param main Optional plot title.
+#' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
 #'
-#' @details 
-#' This function displays the results of a differential expression or methylation 
-#' test as an MD plot, with colors distinguishing probes that meet the 
+#' @details
+#' This function displays the results of a differential expression or methylation
+#' test as an MD plot, with colors distinguishing probes that meet the
 #' user-defined FDR threshold. These figures can be used to evaluate the symmetry,
-#' magnitude, and significance of fold changes for a given experiment. 
-#' 
+#' magnitude, and significance of fold changes for a given experiment.
+#'
 #' @examples
 #' library(dplyr)
 #' df <- data_frame(logFC   = c(rnorm(50, 0, 10), rnorm(4950)),
@@ -23,7 +23,7 @@
 #'                  p.value = pnorm(-abs(logFC)),
 #'                  FDR     = p.adjust(p.value, method = "fdr"))
 #' plot_md(df)
-#' 
+#'
 #' library(limma)
 #' DE_genes <- cbind(matrix(rnorm(250, 5, 1), nrow = 50, ncol = 5),
 #'                   matrix(rnorm(250), nrow = 50, ncol = 5))
@@ -40,13 +40,13 @@
 #' @import ggplot2
 #'
 
-plot_md <- function(dat, 
-                    fdr    = 0.05, 
+plot_md <- function(dat,
+                    fdr    = 0.05,
                     ptsize = 0.25,
-                    main   = NULL, 
+                    main   = NULL,
                     legend = 'outside') {
 
-  dat <- as_tibble(na.omit(dat))
+  dat <- as_data_frame(na.omit(dat))
   if ('adj.P.Val' %in% colnames(dat)) {
     dat <- dat %>% rename(q.value = adj.P.Val)
   }
@@ -60,8 +60,8 @@ plot_md <- function(dat,
       !'FDR' %in% colnames(dat) &
       !'padj' %in% colnames(dat) &
       !'q.value' %in% colnames(dat)) {
-    stop('dat must include a column for adjusted p-values. Recognized colnames 
-  for this vector include "adj.P.Val", "padj", "FDR", and "q.value". Make sure 
+    stop('dat must include a column for adjusted p-values. Recognized colnames
+  for this vector include "adj.P.Val", "padj", "FDR", and "q.value". Make sure
   that dat includes exactly one such colname.')
   }
   if ('AvgMeth' %in% colnames(dat)) {
@@ -78,11 +78,11 @@ plot_md <- function(dat,
   }
   if (!'AveExpr' %in% colnames(dat) &
       !'logCPM' %in% colnames(dat) &
-      !'baseMean' %in% colnames(dat) & 
+      !'baseMean' %in% colnames(dat) &
       !'AvgExpr' %in% colnames(dat)) {
-    stop('dat must include a column for average expression or methylation by 
-  probe. Recognized colnames for this vector include "AvgExpr", "AvgMeth", 
-  "AveExpr", "logCPM", and "baseMean". Make sure that dat includes exactly one 
+    stop('dat must include a column for average expression or methylation by
+  probe. Recognized colnames for this vector include "AvgExpr", "AvgMeth",
+  "AveExpr", "logCPM", and "baseMean". Make sure that dat includes exactly one
   such colname.')
   }
   if ('log2FoldChange' %in% colnames(dat)) {
@@ -90,8 +90,8 @@ plot_md <- function(dat,
   }
   if (!'log2FoldChange' %in% colnames(dat) &
       !'logFC' %in% colnames(dat)) {
-    stop('dat must include a log fold change column. Recognized colnames for 
-  this vector include "logFC" and "log2FoldChange". Make sure that dat includes 
+    stop('dat must include a log fold change column. Recognized colnames for
+  this vector include "logFC" and "log2FoldChange". Make sure that dat includes
   exactly one such colname.')
   }
 
@@ -105,23 +105,23 @@ plot_md <- function(dat,
     select(AvgExpr, logFC, is.DE)
 
   if (sum(df$is.DE == TRUE) == 0) {
-    warning('dat returned no differentially expressed/methylated probes at your 
-  selected fdr threshold. To color points by differential expression/methylation, 
+    warning('dat returned no differentially expressed/methylated probes at your
+  selected fdr threshold. To color points by differential expression/methylation,
   consider raising your fdr cutoff.')
-    p <- ggplot(df, aes(AvgExpr, logFC)) + 
-      geom_point(size = ptsize, alpha = 0.25) 
+    p <- ggplot(df, aes(AvgExpr, logFC)) +
+      geom_point(size = ptsize, alpha = 0.25)
   } else {
-    p <- ggplot(df, aes(AvgExpr, logFC, color = is.DE)) +  
-      geom_point(size = ptsize, alpha = 0.25) + 
-      scale_colour_manual(name   = expression(italic(q)*'-value'), 
+    p <- ggplot(df, aes(AvgExpr, logFC, color = is.DE)) +
+      geom_point(size = ptsize, alpha = 0.25) +
+      scale_colour_manual(name   = expression(italic(q)*'-value'),
                           labels = c(paste('\u2265', fdr), paste('<', fdr)),
-                          values = c('black', 'red')) + 
+                          values = c('black', 'red')) +
       guides(col = guide_legend(reverse = TRUE))
   }
 
-  p <- p + labs(title = main, 
-    x = expression('log'[2]*' Fold Change'), 
-    y = expression('-log'[10]*' '*italic(p))) + 
+  p <- p + labs(title = main,
+    x = expression('log'[2]*' Fold Change'),
+    y = expression('-log'[10]*' '*italic(p))) +
     theme_bw()
 
   if (legend == 'bottomleft') {
@@ -135,7 +135,7 @@ plot_md <- function(dat,
   }
 
   print(p)
-  
+
 }
 
 
