da1c00cdfee8cd2d44ae81a3eb3fd6c95dd62206
dswatson <david.s.watson11@gmail.com>
2016-11-20 11:44:08 +0000

    plotly

--- R/plot_pca.R
+++ R/plot_pca.R
@@ -1,21 +1,32 @@
-#' Plot the first two principal components of an omic data matrix
+#' Plot the principal components of an omic data matrix
 #'
 #' @param dat Omic data matrix with rows corresponding to probes and columns
 #'   to samples. It is strongly recommended that data be filtered and
 #'   normalized prior to running PCA.
 #' @param group Optional factor or character vector of length equal to sample size.
-#'   Levels are used to color and shape points.
-#' @param label Label data points by sample name? Defaults to FALSE unless
+#'   Levels are used to color and shape points. If supplied, legend title defaults to
+#'   "Group". Override this feature by passing a named list instead.
+#' @param label Label data points by sample name? Defaults to \code{FALSE} unless
 #'   \code{group = NULL}.
 #' @param main Optional plot title.
 #' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
+#' @param hover Show sample name by hovering mouse over data point? Renders
+#'   the plot in HTML, which opens the graphic display in your browser. The plot can
+#'   also be embedded in an HTML doc using Rmarkdown so long as \code{knitr = TRUE}
+#'   and you set the code chunk option \code{plotly = TRUE}.
+#' @param D3 Render the plot in three dimensions? Like \code{hover}, this creates a
+#'   plotly object that opens in your browser or can be embedded in an HTML doc.
+#' @param knitr Set this to \code{TRUE} if you want to embed a plotly object (i.e.,
+#'   the \code{plot_pca} output when \code{hover = TRUE} or \code{D3 = TRUE}) in
+#'   an HTML doc. Make sure to set \code{plotly = TRUE} in the corresponding code
+#'   chunk options.
 #'
 #' @details
-#' This function plots the first two principal components of an omic data
-#' matrix. The numbers printed in the x- and y-axis labels indicate the
-#' percentage of variance explained by each component. PCA is an easy and
-#' popular method for unsupervised cluster detection. It can also aid in
+#' This function plots the samples of an omic data matrix in a two- or three-
+#' dimensional principal component subspace. The numbers printed along the axis
+#' labels indicate the percentage of variance explained by each component. PCA is an
+#' easy and popular method for unsupervised cluster detection. It can also aid in
 #' spotting potential outliers and generally  help to visualize the latent
 #' structure of a data set.
 #'
@@ -33,20 +44,30 @@
 #' @export
 #' @import dplyr
 #' @import ggplot2
+#' @import plotly
 #'
 
 plot_pca <- function(dat,
                      group  = NULL,
                      label  = FALSE,
                      main   = NULL,
-                     legend = 'outside') {
+                     legend = 'outside',
+                     hover  = FALSE,
+                     D3     = FALSE,
+                     knitr  = FALSE) {
 
+  # Preliminaries
   if (is.null(group)) {
     group <- rep(1, ncol(dat))
   } else {
-    if (!is.character(group) &
-        !is.factor(group)) {
-      stop('group must be a character or factor variable.')
+    if (!is.list(group)) {
+      if (!is.character(group) & !is.factor(group)) {
+        stop('group must be a character or factor variable.')
+      }
+    } else {
+      if (!is.character(group[[1]]) & !is.factor(group[[1]])) {
+        stop('group must be a character or factor variable.')
+      }
     }
   }
   if (is.null(colnames(dat))) {
@@ -58,14 +79,23 @@ plot_pca <- function(dat,
     main <- 'PCA'
   }
 
+  # PCA
   pca <- prcomp(t(dat), center = TRUE, scale. = TRUE)
   var1 <- round(pca$sdev[1]^2 / sum(pca$sdev^2) * 100, 2)
   var2 <- round(pca$sdev[2]^2 / sum(pca$sdev^2) * 100, 2)
+
+  # Tidy
   df <- data_frame(PC1    = pca$x[, 1],
                    PC2    = pca$x[, 2],
-                   Sample = sample,
-                   Group  = group)
+                   PC3    = pca$x[, 3],
+                   Sample = sample)
+  if (!is.list(group)) {
+    df <- df %>% mutate(Group = group)
+  } else {
+    df <- df %>% mutate(Group = group[[1]])
+  }
 
+  # Basic plot
   p <- ggplot(df, aes(PC1, PC2)) +
     geom_hline(yintercept = 0, size = 0.2) +
     geom_vline(xintercept = 0, size = 0.2) +
@@ -75,25 +105,35 @@ plot_pca <- function(dat,
     theme_bw() +
     theme(plot.title = element_text(hjust = .5))
 
+  # Sample labels
   if (label == TRUE) {
     if (!is.numeric(df$Group)) {
-      p <- p + geom_text(aes(label = Sample, color = Group))
+      p <- p + geom_text(aes(label = Sample, color = Group, alpha = 0.85))
     } else {
-      p <- p + geom_text(aes(label = Sample))
+      p <- p + geom_text(aes(label = Sample, alpha = 0.85))
     }
   } else {
     if (!is.numeric(df$Group)) {
-      p <- p + geom_point(aes(color = Group, shape = Group))
+      suppressWarnings(p <- p + geom_point(aes(text  = paste('Sample:', Sample),
+                                               color = Group, shape = Group),
+                                           alpha = 0.85))
     } else {
       p <- p + geom_text(aes(label = Sample))
     }
   }
 
+  # Named list?
+  if (!is.null(names(group)) & !is.numeric(df$Group)) {
+    p <- p + guides(color = guide_legend(title = names(group)),
+                    shape = guide_legend(title = names(group)))
+  }
+
+  # Locate legend
   if (legend == 'bottomleft') {
     p <- p + theme(legend.justification = c(0.01, 0.01),
                    legend.position = c(0.01, 0.01))
   } else if (legend == 'bottomright') {
-    p <- p + theme(legend.justification = c(1, 0.01),
+    p <- p + theme(legend.justification = c(0.99, 0.01),
                    legend.position = c(0.99, 0.01))
   } else if (legend == 'topleft') {
     p <- p + theme(legend.justification = c(0.01, 0.99),
@@ -103,7 +143,34 @@ plot_pca <- function(dat,
                    legend.position = c(0.99, 0.99))
   }
 
-  print(p)
+  # Output
+  if (D3 == FALSE) {
+    if (hover == FALSE) {
+      print(p)
+    } else {
+      if (knitr == FALSE) {
+        p <- ggplotly(p, tooltip = 'text', width = 600, height = 500)
+        print(p)
+      } else {
+        p <- ggplotly(p, tooltip = 'text', width = 600, height = 500,
+                      session = 'knitr')
+        print(p)
+      }
+    }
+  } else {
+      p <- plot_ly(df, x = ~PC1, y = ~PC2, z = ~PC3,
+                   text = ~Sample, color = ~Group, symbol = ~Group,
+                   colors = c('#E41A1C', '#4DAF4A', '#377EB8'),
+                   symbols = c('circle', 'triangle-up', 'square'),
+                   type = 'scatter3d', mode = 'markers',
+                   alpha = 0.85, hoverinfo = 'text', marker = list(size = 5)) %>%
+        layout(title = main, hovermode = 'closest', scene = list(
+          xaxis = list(title = paste0('PC1 (', var1, '%)')),
+          yaxis = list(title = paste0('PC2 (', var2, '%)')),
+          zaxis = list(title = paste0('PC3 (', var3, '%)'))
+        ))
+      print(p)
+  }
 
 }
 
