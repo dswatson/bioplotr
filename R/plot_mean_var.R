202b305aac84022e8da3aceac66a38fc99d00761
dswatson <david.s.watson11@gmail.com>
2016-11-23 11:08:17 +0000

    omic genpop

--- R/plot_mean_var.R
+++ R/plot_mean_var.R
@@ -1,23 +1,26 @@
-#' Plot the mean-variance trend of a gene expression matrix
+#' Plot the mean-variance trend of an omic data matrix
 #'
-#' @param dat Matrix of log expression intensities or raw counts.
+#' @param dat Omic data matrix with rows corresponding to probes and columns
+#'   to samples.
 #' @param type String specifying data type. Must be one of either
 #'   \code{"microarray"} or \code{"RNA-seq"}.
 #' @param main Optional plot title.
 #' @param hover Show probe name by hovering mouse over data point? If \code{TRUE},
 #'   the plot is rendered in HTML and will either open in your browser's graphic
 #'   display or appear in the RStudio viewer.
+#' @param probes String specifying the name of the column in which to find the probe
+#'   identifiers. Only relevant if \code{hover = TRUE}.
 #'
 #' @details
-#' This function plots each gene's mean expression against either the
-#' logarithm of its standard deviation (if \code{type = "microarray"}) or
-#' its square root (if \code{type = "RNA-seq"}). A lowess curve is additionally
-#' fit to the data.
+#' This function plots each probe's mean value against either the logarithm of
+#' its standard deviation (if \code{type = "microarray"}) or its square root
+#' (if \code{type = "RNA-seq"}). A lowess curve is additionally fit to the data.
 #'
 #' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_mean_var(mat, type = "microarray")
 #'
+#' library(edgeR)
 #' mat <- matrix(rnbinom(5000, mu = 4, size = 1), nrow = 1000, ncol = 5)
 #' mat <- cpm(y, log = TRUE)
 #' plot_mean_var(mat, type = "RNA-seq")
@@ -34,13 +37,12 @@ plot_mean_var <- function(dat,
                           ptsize = 0.25,
                           main   = NULL,
                           hover  = FALSE,
-                          knitr  = FALSE) {
+                          probes = NULL) {
 
-  if (is.null(type) |
-      !type %in% c('microarray', 'RNA-seq')) {
+  # Preliminaries
+  if (!type %in% c('microarray', 'RNA-seq')) {
     stop('type must be specified as either "microarray" or "RNA-seq".')
   }
-
   if (type == 'microarray') {
     vars <- log2(rowSds(dat))
     ylab <- expression('log'[2]*sigma)
@@ -51,36 +53,33 @@ plot_mean_var <- function(dat,
     vars <- sqrt(rowSds(dat))
     ylab <- expression(sqrt(sigma))
     if (is.null(main)) {
-      main <- expression('log'[2]*' Counts Per Million')
+      main <- expression('log'[2]*' Counts Per Million') #MREH
     }
   }
 
-  df <- data_frame(Gene = rownames(dat),
-                   Mean = rowMeans(dat),
-                   Var  = vars)
+  # Tidy
+  df <- data_frame(Probe = rownames(dat),
+                   Mean  = rowMeans(dat),
+                   Var   = vars)
   lo <- lowess(x = df$Mean, y = df$Var, f = 0.5)
   df <- df %>% mutate(lo.x = lo[['x']],
                       lo.y = lo[['y']])
 
+  # Build plot
   p <- ggplot(df) +
-    suppressWarnings(geom_point(aes(Mean, Var, text = paste('Gene:', Gene)),
+    suppressWarnings(geom_point(aes(Mean, Var, text = Probe),
                                 size = ptsize, alpha = 0.25)) +
     geom_smooth(aes(lo.x, lo.y), size = 0.5) +
     labs(title = main, x = expression(mu), y = ylab) +
     theme_bw() +
     theme(plot.title = element_text(hjust = .5))
 
+  # Output
   if (hover == FALSE) {
     print(p)
   } else {
-    if (knitr == FALSE) {
-      p <- ggplotly(p, tooltip = 'text', width = 600, height = 500)
-      print(p)
-    } else {
-      p <- ggplotly(p, tooltip = 'text', width = 600, height = 500,
-                    session = 'knitr')
-      print(p)
-    }
+    p <- ggplotly(p, tooltip = 'text', height = 600, width = 600)
+    print(p)
   }
 
 }
