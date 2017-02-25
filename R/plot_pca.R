26b900e3ad099b5d951e9c79c543d8caa3160196
dswatson <david.s.watson11@gmail.com>
2017-02-25 13:34:38 +0000

    up to two covars, continuous ok

--- R/plot_pca.R
+++ R/plot_pca.R
@@ -2,14 +2,16 @@
 #'
 #' This function plots the principal components of an omic data matrix.
 #'
-#' @param dat Omic data matrix with rows corresponding to probes and columns
-#'   to samples. It is strongly recommended that data be normalized and
-#'   filtered prior to running PCA.
-#' @param group Optional factor or character vector of length equal to sample size.
-#'   Levels are used to color and shape points. If supplied, legend title defaults to
-#'   "Group". Override this feature by passing a named list instead.
+#' @param dat Omic data matrix or matrix-like object with rows corresponding to
+#'   probes and columns to samples. It is strongly recommended that data be
+#'   normalized and filtered prior to running PCA.
+#' @param covar Optional vector of length equal to sample size, or up to two such
+#'   vectors organized into a list or data frame. If passing two covariates, only one
+#'   may be continuous. Supply legend title(s) by passing a named list or data frame.
+#' @param top Optional number (if > 1) or proportion (if < 1) of most variable probes
+#'   to be used for PCA.
 #' @param label Label data points by sample name? Defaults to \code{FALSE} unless
-#'   \code{group = NULL}.
+#'   \code{covar = NULL}. If \code{TRUE}, then plot can render at most one covariate.
 #' @param main Optional plot title.
 #' @param legend Legend position. Must be one of \code{"outside",
 #'   "bottomleft", "bottomright", "topleft",} or \code{"topright"}.
@@ -25,6 +27,10 @@
 #' unsupervised cluster detection. It can also aid in spotting potential outliers
 #' and generally helps to visualize the latent structure of a data set.
 #'
+#' By default, \code{plot_pca} performs singular value decomposition on the complete
+#' \code{dat} matrix. Limit the PCA to only the most variable probes by using the
+#' \code{top} argument.
+#'
 #' @examples
 #' mat <- matrix(rnorm(5000), nrow = 1000, ncol = 5)
 #' plot_pca(mat)
@@ -34,18 +40,24 @@
 #'              matrix(rnbinom(5000, mu = 4, size = 10), nrow = 1000, ncol = 5))
 #' mat <- rlog(mat)
 #' grp <- gl(n = 2, k = 5, labels = c("A", "B"))
-#' plot_pca(mat, group = grp)
+#' plot_pca(mat, covar = grp)
+#'
+#' @seealso
+#' \code{\link{plot_mds}} \code{\link[DESeq2]{plotPCA}}
 #'
 #' @export
+#' @importFrom limma getEAWP
 #' @import dplyr
-#' @importFrom purrr map_dbl
+#' @importFrom purrr map map_chr
+#' @importFrom matrixStats rowVars
 #' @import ggplot2
 #' @import plotly
 #' @importFrom scales hue_pal
 #'
 
 plot_pca <- function(dat,
-                     group = NULL,
+                     covar = NULL,
+                       top = NULL,
                      label = FALSE,
                       main = NULL,
                     legend = 'outside',
@@ -53,29 +65,82 @@ plot_pca <- function(dat,
                         D3 = FALSE) {
 
   # Preliminaries
-  if (is.null(colnames(dat))) {
-    sample <- paste0('Sample', seq_along(dat))
-  } else {
-    sample <- colnames(dat)
+  dat <- getEAWP(dat)
+  dat <- dat$expr
+  bad <- rowSums(is.finite(dat)) < ncol(dat)
+  if (any(bad)) {
+    dat <- dat[!bad, , drop = FALSE]
   }
-  if (is.null(group)) {
-    group <- list(rep(1, times = ncol(dat)))
-  } else {
-    if (!is.list(group)) {
-      group <- list(group)
+  if (ncol(dat) < 3) {
+    stop(paste('dat includes only', ncol(dat), 'samples; need at least 3 for PCA.'))
+  }
+  if (is.null(rownames(dat))) {
+    rownames(dat) <- seq_len(nrow(dat))
+  }
+  if (is.null(colnames(dat))) {
+    colnames(dat) <- paste0('Sample', seq_len(ncol(dat)))
+  }
+  if (!is.null(covar)) {
+    if (is.data.frame(covar)) {
+      covar <- as.list(covar)
+    } else if (!is.list(covar)) {
+      covar <- list(covar)
     }
-    if (!is.character(group[[1]]) & !is.factor(group[[1]])) {
-      stop('group must be a character or factor variable')
+    if (length(covar) > 2) {
+      stop('covar cannot contain more than two covariates.')
     }
-    if (length(group) > 1) {
-      stop('group cannot be a list of length > 1')
+    for (i in seq_along(covar)) {
+      if (length(covar[[i]]) != ncol(dat)) {
+        stop('Covariate(s) must be of length equal to sample size.')
+      }
+      if (is.numeric(covar[[i]]) && var(covar[[i]]) == 0) {
+        warning('Continuous feature is invariant.')
+      } else if (!is.numeric(covar[[i]]) && length(unique(covar[[i]])) == 1) {
+        warning('Grouping factor is invariant.')
+      }
     }
-    if (length(group[[1]]) != ncol(dat)) {
-      stop('group length must match number of samples in dat')
+    nums <- as.logical(map(covar, is.numeric))
+    if (sum(nums) == 2) {
+      stop('Only one continuous covariate can be plotted at a time.')
     }
-    if (length(unique(group[[1]])) == 1) {
-      warning('group is invariant')
+    if (any(nums)) {
+      cont_cov <- TRUE
+      if (which(nums) == 2) {
+        covar <- covar[c(2, 1)]
+      }
+    } else {
+      cont_cov <- FALSE
     }
+    if (!is.null(names(covar))) {
+      covars <- names(covar)
+    } else {
+      if (cont_cov) {
+        covars <- c('Feature', 'Group')
+      } else {
+        if (length(covar) == 1) {
+          covars <- 'Group'
+        } else {
+          covars <- c('Factor 1', 'Factor 2')
+        }
+      }
+    }
+    names(covar) <- paste0('Feature', seq_along(covar))
+    covar <- tbl_df(covar) %>% mutate(Sample = colnames(dat))
+  }
+  if (!is.null(top)) {
+    if (top > 1) {
+      if (top > nrow(dat)) {
+        warning('top exceeds nrow(dat), at least after removing probes with infinite ',
+                'or missing values. Proceeding with the complete matrix.')
+      }
+    } else {
+      top <- round(top * nrow(dat))
+    }
+    vars <- rowVars(dat)
+    dat <- dat[rev(order(vars))[seq_len(top)], , drop = FALSE]
+  }
+  if (label && length(covars) == 2) {
+    stop('If label is TRUE, then plot can render at most one covariate.')
   }
   if (is.null(main)) {
     main <- 'PCA'
@@ -86,43 +151,54 @@ plot_pca <- function(dat,
   }
 
   # Tidy data
-  pca <- prcomp(t(dat))               # PCA
-  pve <- map_dbl(1:3, function(pc) {
-    round(pca$sdev[pc]^2 / sum(pca$sdev^2) * 100, 2)
+  pca <- prcomp(t(dat))                     # PCA
+  pve <- map_chr(seq_len(3), function(pc) {
+    p <- round(pca$sdev[pc]^2 / sum(pca$sdev^2) * 100, 2)
+    paste0('PC', pc, ' (', p, '%)')
   })
-  df <- data_frame(PC1 = pca$x[, 1],  # Melt
-                   PC2 = pca$x[, 2],
-                   PC3 = pca$x[, 3],
-                Sample = sample,
-                 Group = group[[1]])
+  df <- data_frame(Sample = colnames(dat),  # Melt
+                      PC1 = pca$x[, 1],
+                      PC2 = pca$x[, 2],
+                      PC3 = pca$x[, 3])
+  if (!is.null(covar)) {
+    df <- inner_join(df, covar, by = 'Sample')
+  }
 
   # Build plot
   p <- ggplot(df, aes(PC1, PC2)) +
     geom_hline(yintercept = 0, size = 0.2) +
     geom_vline(xintercept = 0, size = 0.2) +
-    labs(title = main,
-             x = paste0('PC1 (', pve[1], '%)'),
-             y = paste0('PC2 (', pve[2], '%)')) +
+    labs(title = main, x = pve[1], y = pve[2]) +
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5))
-  if (!is.numeric(df$Group)) {        # Sample labels
+  if (ncol(covar) == 2) {
     if (label) {
-      p <- p + geom_text(aes(label = Sample, color = Group),
+      p <- p + geom_text(aes(label = Sample, color = Feature1),
                          alpha = 0.85)
     } else {
-      suppressWarnings(
-        p <- p + geom_point(aes(text = Sample, color = Group, shape = Group),
-                            alpha = 0.85)
-      )
+      if (cont_cov) {
+        suppressWarnings(
+          p <- p + geom_point(aes(text = Sample, color = Feature1),
+                              alpha = 0.85)
+        )
+      } else {
+        suppressWarnings(
+          p <- p + geom_point(aes(text = Sample, color = Feature1, shape = Feature1),
+                              alpha = 0.85)
+        )
+      }
     }
+    p <- p + guides(color = guide_legend(title = covars[1]),
+                    shape = guide_legend(title = covars[1]))
   } else {
-    p <- p + geom_text(aes(label = Sample), alpha = 0.85)
+    suppressWarnings(
+      p <- p + geom_point(aes(text = Sample, color = Feature1, shape = Feature2),
+                          alpha = 0.85) +
+        guides(color = guide_legend(title = covars[1]),
+               shape = guide_legend(title = covars[2]))
+    )
   }
-  if (!is.null(names(group))) {       # Named list?
-    p <- p + guides(color = guide_legend(title = names(group)),
-                    shape = guide_legend(title = names(group)))
-  }
-  if (legend == 'bottomleft') {       # Locate legend
+  if (legend == 'bottomleft') {             # Locate legend
     p <- p + theme(legend.justification = c(0.01, 0.01),
                    legend.position = c(0.01, 0.01))
   } else if (legend == 'bottomright') {
@@ -141,11 +217,16 @@ plot_pca <- function(dat,
     if (!hover) {
       print(p)
     } else {
-      p <- ggplotly(p, tooltip = 'text', height = 600, width = 600)
+      if (legend == 'outside') {
+        p <- ggplotly(p, tooltip = 'text', height = 525, width = 600)
+      } else {
+        p <- ggplotly(p, tooltip = 'text', height = 600, width = 600)
+      }
       print(p)
     }
   } else {
-    # symbls <- c(16, 17, 15, 3, 7, 8)  # This would be right if plotly worked
+    ### REWRITE ###
+    # symbls <- c(16, 17, 15, 3, 7, 8)      # This would be right if plotly worked
     symbls <- c(16, 18, 15, 3, 7, 8)
     p <- plot_ly(df, x = ~PC1, y = ~PC2, z = ~PC3,
                  text = ~Sample, color = ~Group, symbol = ~Group,
@@ -154,16 +235,12 @@ plot_pca <- function(dat,
                  type = 'scatter3d', mode = 'markers',
                  alpha = 0.85, hoverinfo = 'text', marker = list(size = 5)) %>%
       layout(hovermode = 'closest', title = main, scene = list(
-             xaxis = list(title = paste0('PC1 (', vars[1], '%)')),
-             yaxis = list(title = paste0('PC2 (', vars[2], '%)')),
-             zaxis = list(title = paste0('PC3 (', vars[3], '%)'))))
+             xaxis = list(title = pve[1]),
+             yaxis = list(title = pve[2]),
+             zaxis = list(title = pve[3])))
     print(p)
   }
 
 }
 
-
-
-# Allow for continuous covariate
-
-
+# Argument to pick which PCs to plot
