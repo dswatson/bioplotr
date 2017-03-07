c4d6c3b1f54f472745a9dae2882b1bb3e6a26789
dswatson <david.s.watson11@gmail.com>
2017-03-07 16:26:02 -0500

    enter quant

---
+++ R/plot_quantiles.R
@@ -0,0 +1,82 @@
+#' Plot Quantiles
+#'
+#' This function plots the quantiles for two vectors against each other as either a
+#' QQ or an MD plot.
+#'
+#' @param x Vector of numeric values.
+#' @param y Second vector of numeric values.
+#' @param method Plot quantiles against quantiles (\code{method = "QQ"}) or mean
+#'   quantiles against difference in quantiles (\code{method = "MD"})?
+#' @param pts Number of points to plot.
+#' @param main Optional plot title.
+#' @param xlab Optional x-axis label.
+#' @param ylab Optional y-axis label.
+#'
+#' @details
+#' QQ and MD plots visualize the relationship between two numeric vectors. They are
+#' a quick and easy alternative to scatterplots when distributions are of unequal
+#' length.
+#'
+#' @examples
+#' x1 <- rnorm(100)
+#' x2 <- runif(500)
+#' plot_quantiles(x1, x2)
+#' plot_quantiles(x1, x2, method = "MD")
+#'
+#' @export
+#' @importFrom dplyr data_frame
+#' @import ggplot2
+#'
+
+
+plot_quantiles <- function(x,
+                           y,
+                           method = 'QQ',
+                           pts = 1e3L,
+                           main = NULL,
+                           xlab = NULL,
+                           ylab = NULL) {
+
+  # Preliminaries
+  x <- x[is.finite(x)]
+  if (length(x) < 1L) {
+    stop('x must have at least one finite, non-missing value.')
+  }
+  y <- y[is.finite(y)]
+  if (length(y) < 1L) {
+    stop('y must have at least one finite, non-missing value.')
+  }
+  if (!method %in% c('QQ', 'MD')) {
+    stop('method must be either "QQ" or "MD".')
+  }
+  if (is.null(main)) {
+    if (method == 'QQ') main <- 'QQ Plot'
+    else main <- 'MD Plot'
+  }
+  if (is.null(xlab)) xlab <- 'X'
+  if (is.null(ylab)) ylab <- 'Y'
+
+  # Tidy data
+  x <- quantile(x, probs = seq(0L, 1L, length.out = pts))
+  y <- quantile(y, probs = seq(0L, 1L, length.out = pts))
+  if (method == 'QQ') df <- data_frame(X = x, Y = y)
+  else df <- data_frame(X = (x + y) / 2L, Y = x - y)
+
+  # Build plot
+  p <- ggplot(df, aes(X, Y)) +
+    geom_point(size = 0.5) +
+    labs(title = main, x = xlab, y = ylab) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5))
+
+  if (method == 'QQ') {
+    p <- p + geom_abline(intercept = 0L, slope = 1L, color = 'red', size = 0.2)
+  } else {
+    p <- p + geom_hline(yintercept = 0L, color= 'red', size = 0.2)
+  }
+
+  print(p)
+
+}
+
+
