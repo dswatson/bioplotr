1431739bccd592c3ad569a856fc5a6a6e48c1b8d
dswatson <david.s.watson11@gmail.com>
2017-04-08 12:52:04 +0100

    s3 for days

--- R/plot_dispersion.R
+++ R/plot_dispersion.R
@@ -5,33 +5,41 @@
 #' @param dat A \code{\link[edgeR]{DGEList}} object or \code{\link[DESeq2]{
 #'   DESeqDataSet}}. If normalization factors or dispersions have not already been
 #'   estimated for \code{dat}, they will be internally computed using the appropriate
-#'   \code{edgeR} or \code{DESeq2} functions. For the former, a design matrix may be
-#'   required to calculate the genewise adjusted profile log-likelihoods. See
-#'   Details.
-#' @param design Optional study design matrix. This will be extracted from \code{dat}
+#'   \code{edgeR} or \code{DESeq2} functions. Alternatively, \code{dat} may be a raw
+#'   count matrix, in which case the \code{pipeline} argument must specify which
+#'   software to use for estimating normalization factors and genewise dispersions.
+#'   A design matrix may be required to calculate adjusted profile log-likelihoods.
+#'   See Details.
+#' @param design Optional design matrix with rows corresponding to samples and columns
+#'   to coefficients to be estimated. This will be extracted from \code{dat}
 #'   if available and need not be set explicitly in the call. If provided, however,
-#'   \code{design} will override the relevant slots or components of \code{dat}.
+#'   \code{design} will override the relevant slot of \code{dat}.
 #' @param trans Data transformation to be applied to genewise dispersion estimates.
-#'  Must be one of \code{"log"} or \code{"sqrt"}.
+#'   Must be one of \code{"log"} or \code{"sqrt"}.
+#' @param pipeline Which package should be used to estimate normalization factors
+#'   and genewise dispersions? Only relevant if \code{dat} is a raw count matrix.
+#'   Must be one of \code{"edgeR"} or \code{"DESeq2"}. Default settings are applied
+#'   at all steps. For greater control of internal parameters, create the appropriate
+#'   \code{DGEList} or \code{DESeqDataSet} object with your preferred settings and
+#'   pass it directly as \code{dat}.
 #' @param title Optional plot title.
 #' @param legend Legend position. Must be one of \code{"outside", "bottomleft",
 #'   "bottomright", "topleft",} or \code{"topright"}.
 #' @param hover Show probe name by hovering mouse over data point? If \code{TRUE},
 #'   the plot is rendered in HTML and will either open in your browser's graphic
-#'   display or appear in the RStudio viewer. Gene names are extracted from
-#'   \code{dat}.
+#'   display or appear in the RStudio viewer. Gene names are extracted from \code{dat}.
 #'
 #' @details
 #' Count data in omic studies are often presumed to follow a negative binomial
 #' distribution, which may be uniquely identified by its mean and dispersion
 #' parameters. For RNA-seq pipelines that rely on negative binomial generalized linear
 #' models (GLMs), such as \code{edgeR} and \code{DESeq2}, estimating genewise
-#' dispersions is an essential step in the model fitting process. There are rarely
-#' sufficient replicates to reliably infer these values independently for each gene,
-#' however, which is why both packages use empirical Bayes methods to pool information
-#' across genes.
+#' dispersions is therefore an essential step in the model fitting process. Becaeuse
+#' there are rarely sufficient replicates to reliably infer these values independently
+#' for each gene, both packages use empirical Bayes methods to pool information across
+#' genes.
 #'
-#' Details vary across the two pipelines, which is why output will differ for \code{
+#' Details vary between the two pipelines, which is why output will differ for \code{
 #' DGEList} objects and \code{DESeqDataSet}s. \code{edgeR} begins by computing a common
 #' dispersion parameter for the entire dataset, rendered by \code{plot_dispersion} as
 #' a blue horizontal line; then fits a trend line to the maximized negative binomial
@@ -53,8 +61,8 @@
 #' while outliers, defined as genes with log dispersion values more than two median
 #' absolute deviations away from the trend line, are colored red. See \code{
 #' \link[DESeq2]{estimateDispersions}} for more details. For more comprehensive
-#' statistical background, see the original DESeq paper (Anders & Huber, 2010), as
-#' well as the DESeq2 paper (Love et al., 2014).
+#' statistical background, see the original DESeq paper (Anders & Huber, 2010) and
+#' the DESeq2 paper (Love et al., 2014).
 #'
 #' \code{plot_dispersion} effectively combines \code{edgeR::\link[edgeR]{plotBCV}}
 #' and \code{DESeq2::\link[DESeq2]{plotDispEsts}} into a single function that can
@@ -95,14 +103,15 @@
 #' dds <- makeExampleDESeqDataSet()
 #' plot_dispersion(dds)
 #'
+#' # Plot the same data using the edgeR pipeline and sqrt transform
+#' plot_dispersion(counts(dds), design = model.matrix(~ condition, colData(dds)),
+#'                 pipeline = "edgeR", trans = "sqrt")
+#'
 #' @seealso
 #' \code{\link[DESeq2]{plotDispEsts}, \link[edgeR]{plotBCV},
 #' \link[DESeq2]{estimateDispersions}, \link[edgeR]{estimateDisp}}
 #'
 #' @export
-#' @importFrom DESeq2 sizeFactors normalizationFactors estimateSizeFactors
-#'   estimateDispersions counts dispersions
-#' @importFrom edgeR calcNormFactors estimateDisp aveLogCPM
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom ggsci pal_d3
@@ -110,36 +119,11 @@
 #'
 
 plot_dispersion <- function(dat,
-                            design = NULL,
-                             trans = 'log',
-                             title = NULL,
-                            legend = 'outside',
-                             hover = FALSE) {
+                            trans = 'log',
+                            title = NULL,
+                           legend = 'outside', ...) {
 
   # Preliminaries
-  if (is(dat, 'DESeqDataSet')) {
-    if (is.null(sizeFactors(dat)) & is.null(normalizationFactors(dat))) {
-      dat <- estimateSizeFactors(dat)
-    }
-    if (!is.null(design)) {
-      dat <- estimateDispersions(dat, modelMatrix = design, quiet = TRUE)
-    } else if (is.null(dispersions(dat))) {
-      dat <- estimateDispersions(dat, quiet = TRUE)
-    }
-  } else if (is(dat, 'DGEList')) {
-    if (all(dat$samples$norm.factors == 1L)) {
-      dat <- calcNormFactors(dat)
-    }
-    if (any(c(is.null(dat$common.dispersion), is.null(dat$trended.dispersion),
-              is.null(dat$tagwise.dispersion)))) {
-      if (is.null(design) & !is.null(dat$design)) {
-        design <- dat$design
-      }
-      dat <- estimateDisp(dat, design = design)
-    }
-  } else {
-    stop('dat must be a DESeqDataSet or DGEList object.')
-  }
   if (trans == 'log') {
     fn <- log10
     ylab <- expression(log[10]~'Dispersion')
@@ -157,57 +141,60 @@ plot_dispersion <- function(dat,
          '"topleft", or "topright".')
   }
 
-  # Tidy data
-  if (is(dat, 'DESeqDataSet')) {
-    keep <- mcols(dat)$baseMean > 0L
-    dat <- dat[keep, , drop = FALSE]
-    cnts <- DGEList(counts(dat))
-    if (is.null(sizeFactors(dat))) {             # Need size factors for aveLogCPM calculation
-      cnts <- calcNormFactors(cnts, method = 'RLE')
-    } else {
-      cnts$samples$norm.factors <- sizeFactors(dat)
-    }
-    df <- data_frame(Gene = rownames(dat),
-                     Mean = aveLogCPM(cnts, prior.count = 1L,
-                                      dispersion = dispersions(dat)),
-                 Genewise = fn(mcols(dat)$dispGeneEst),
-                      Fit = fn(mcols(dat)$dispFit),
-                    Final = fn(dispersions(dat)),
-                  Outlier = mcols(dat)$dispOutlier)
-  } else {
-    cmn <- fn(dat$common.dispersion)
-    df <- data_frame(Gene = rownames(dat),
-                     Mean = aveLogCPM(dat, prior.count = 1L),
-                 Genewise = fn(dat$tagwise.dispersion),
-                      Fit = fn(dat$trended.dispersion),
-                  Tagwise = rep(TRUE, nrow(dat))) %>%
-      arrange(Mean)
+  # Method
+  UseMethod('plot_mv')
+
+}
+
+
+#' @rdname plot_dispersion
+#' @method plot_dispersion DGEList
+#' @S3method plot_dispersion DGEList
+#' @importFrom edgeR calcNormFactors estimateDisp aveLogCPM
+
+plot_dispersion.DGEList <- function(dat,
+                                    design = NULL,
+                                     trans = 'log',
+                                     title = NULL,
+                                    legend = 'outside',
+                                     hover = FALSE) {
+
+  # Preliminaries
+  if (is.null(dat$samples$norm.factors) |
+      all(dat$samples$norm.factors == 1L)) {
+    dat <- calcNormFactors(dat)
   }
+  if (is.null(dat$tagwise.dispersion)) {
+    if (is.null(dat$design) & !is.null(dat$group)) {
+      dat$design <- model.matrix(~ dat$group)
+    }
+    if (!is.null(design)) {
+      dat$design <- design
+    }
+    if (is.null(dat$design)) {
+      dat <- estimateTagwiseDisp(dat, dispersion = estimateCommonDisp(dat))
+    } else {
+      dat <- estimateDisp(dat)
+    }
+  }
+
+  # Tidy data
+  cmn <- fn(dat$common.dispersion)
+  df <- data_frame(Gene = rownames(dat),
+                   Mean = aveLogCPM(dat, prior.count = 1L,
+                                    dispersion = dat$tagwise.dispersion),
+               Genewise = fn(dat$tagwise.dispersion),
+                    Fit = fn(dat$trended.dispersion),
+                Tagwise = rep(TRUE, nrow(dat))) %>%
+    arrange(Mean)
 
   # Build plot
   size <- probe_ptsize(df)
   alpha <- probe_alpha(df)
-  p <- ggplot(df) +
-    labs(title = title, x = expression('Mean'~log[2]*'-CPM'), y = ylab) +
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
-  if (is(dat, 'DESeqDataSet')) {
-    p <- p + suppressWarnings(
-        geom_point(aes(Mean, Genewise, text = Gene, color = Outlier),
-                   size = size, alpha = alpha)
-      ) + geom_point(data = df %>% filter(Outlier == FALSE),
-                     aes(Mean, Final, color = 'Final'), size = size, alpha = alpha) +
-      geom_point(aes(Mean, Fit, color = 'Trend'), size = size) +
-      scale_color_manual(name = 'Dispersion\n Estimate',
-                       breaks = c(FALSE, 'Trend', 'Final', TRUE),
-                       labels = c('Genewise', 'Trend', 'Final', 'Outlier'),
-                       values = c('black', pal_d3()(4)[c(1:2, 4)]),
-                        guide = guide_legend(override.aes = list(size = rep(1L, 4L))))
-  } else {
-    p <- p + suppressWarnings(
-        geom_point(aes(Mean, Genewise, text = Gene, color = Tagwise),
-                   size = size, alpha = alpha)
-      ) + geom_hline(aes(color = 'Common', yintercept = cmn), size = 0.5) +
+  suppressWarnings(
+    p <- p + geom_point(aes(Mean, Genewise, text = Gene, color = Tagwise),
+                        size = size, alpha = alpha) +
+      geom_hline(aes(color = 'Common', yintercept = cmn), size = 0.5) +
       geom_path(aes(Mean, Fit, color = 'Trend'), size = 0.5) +
       scale_color_manual(name = 'Dispersion\n Estimate',
                        breaks = c(TRUE, 'Common', 'Trend'),
@@ -215,13 +202,110 @@ plot_dispersion <- function(dat,
                        values = c(pal_d3()(2), 'black'),
                         guide = guide_legend(override.aes = list(
                           linetype = c('blank', rep('solid', 2L)),
-                             shape = c(16L, NA, NA), size = rep(1L, 3L)
-                        )))
-  }
+                             shape = c(16L, NA, NA), size = rep(1L, 3L))))
+  )
   p <- locate_legend(p, legend)
 
   # Output
   gg_out(p, hover, legend)
+
+}
+
+
+#' @rdname plot_dispersion
+#' @method plot_dispersion DESeqDataSet
+#' @S3method plot_dispersion DESeqDataSet
+#' @importFrom DESeq2 sizeFactors normalizationFactors estimateSizeFactors
+#'   dispersions estimateDispersions
+#' @importFrom edgeR aveLogCPM
+
+plot_dispersion.DESeqDataSet <- function(dat,
+                                         trans = 'log',
+                                         title = NULL,
+                                        legend = 'outside',
+                                         hover = FALSE) {
+
+  # Preliminaries
+  if (is.null(sizeFactors(dat)) & is.null(normalizationFactors(dat))) {
+    dat <- estimateSizeFactors(dat)
+  }
+  if (is.null(dispersions(dat))) {
+    dat <- estimateDispersions(dat, quiet = TRUE)
+  }
+
+  # Tidy data
+  keep <- mcols(dat)$baseMean > 0L
+  dat <- dat[keep, , drop = FALSE]
+  df <- data_frame(Gene = rownames(dat),
+                   Mean = aveLogCPM(counts(dat, normalized = TRUE), prior.count = 1L,
+                                    dispersion = dispersions(dat)),
+               Genewise = fn(mcols(dat)$dispGeneEst),
+                    Fit = fn(mcols(dat)$dispFit),
+                  Final = fn(dispersions(dat)),
+                Outlier = mcols(dat)$dispOutlier)
+
+  # Build plot
+  size <- probe_ptsize(df)
+  alpha <- probe_alpha(df)
+  suppressWarnings(
+    p <- ggplot(df) +
+      geom_point(aes(Mean, Genewise, text = Gene, color = Outlier),
+                 size = size, alpha = alpha) +
+      geom_point(data = df %>% filter(Outlier == FALSE),
+                 aes(Mean, Final, color = 'Final'), size = size, alpha = alpha) +
+      geom_point(aes(Mean, Fit, color = 'Trend'), size = size) +
+      scale_color_manual(name = 'Dispersion\n Estimate',
+                       breaks = c(FALSE, 'Trend', 'Final', TRUE),
+                       labels = c('Genewise', 'Trend', 'Final', 'Outlier'),
+                       values = c('black', pal_d3()(4)[c(1:2, 4)]),
+                        guide = guide_legend(override.aes = list(size = rep(1L, 4L)))) +
+      labs(title = title, x = expression('Mean'~log[2]*'-CPM'), y = ylab) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
+  )
+  p <- locate_legend(p, legend)
+
+  # Output
+  gg_out(p, hover, legend)
+
+}
+
+
+#' @rdname plot_dispersion
+#' @method plot_dispersion DESeqDataSet
+#' @S3method plot_dispersion DESeqDataSet
+#' @importFrom DESeq2 sizeFactors normalizationFactors estimateSizeFactors
+#'   dispersions estimateDispersions
+#' @importFrom edgeR aveLogCPM
+
+plot_dispersion.default <- function(dat,
+                                    design = NULL,
+                                     trans = 'log',
+                                  pipeline = NULL,
+                                     title = NULL,
+                                    legend = 'outside',
+                                     hover = FALSE) {
+
+  if (pipeline == 'edgeR') {
+    dat <- DGEList(dat)
+    plot_dispersion(dat, design = design, trans = trans, title = title,
+                    legend = legend, hover = hover)
+  } else if (pipeline == 'DESeq2') {
+    if (is.null(design)) {
+      cd <- data.frame(A = rep(0L, ncol(dat)))
+      dat <- DESeqDataSetFromMatrix(dat, colData = cd, design = ~ 1L)
+      dat <- estimateSizeFactors(dat)
+      dat <- estimateDispersions(dat, quiet = TRUE)
+    } else {
+      dat <- DESeqDataSetFromMatrix(dat, colData = as.data.frame(design),
+                                    design = ~ 1L)
+    }
+    dat <- estimateSizeFactors(dat)
+    dat <- estimateDispersions(dat, quiet = TRUE, modelMatrix = design)
+    plot_dispersion(dat, trans = trans, title = title, legend = legend, hover = hover)
+  } else {
+    stop('pipeline must be one of "edgeR" or "DESeq2".')
+  }
 
 }
 
