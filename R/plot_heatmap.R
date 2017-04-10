5c95683819ca478e0d40c3dd36df7d24adc064f2
dswatson <david.s.watson11@gmail.com>
2017-04-10 15:18:51 +0100

    dist, hclust, new cols

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -4,12 +4,18 @@
 #'
 #' @param dat Omic data matrix or matrix-like object with rows corresponding to
 #'   probes and columns to samples.
-#' @param feat Optional character, factor, numeric, or logical vector of length
+#' @param anno Optional character, factor, numeric, or logical vector of length
 #'   equal to sample size. Alternatively, a data frame or list of such vectors,
 #'   optionally named. Values are used to color one or several annotation tracks
 #'   atop the heatmap.
-#' @param col Color palette to use for heatmap tiles. Options are \code{"RdBu"} (for
-#'   red to blue gradient) or \code{"GrRd"} (for green to red gradient).
+#' @param dist Distance measure to be used. Currently supports any method available
+#'   in \code{\link[stats]{dist}} or \code{\link[stats]{cor}}.
+#' @param hclustfun The agglomeration method to be used for hierarchical clustering.
+#'   See \code{\link[stats]{hclust}} for available options.
+#' @param col Color palette to use for heatmap tiles. Preset options include \code{
+#'   "RdBu"} for red to blue gradient, \code{"GrRd"} for green to red gradient, and
+#'   \code{"BuYl"} for blue to yellow gradient. Alternatively, any user-supplied
+#'   color palette is acceptable.
 #' @param title Optional plot title.
 #'
 #' @details
@@ -20,72 +26,110 @@
 #' used to investigate associations with phenotypic features.
 #'
 #' @examples
-#' mat <- matrix(rnorm(1000 * 10), nrow = 1000, ncol = 10)
+#' mat <- matrix(rnorm(100 * 10), nrow = 100, ncol = 10)
 #' grp <- rep(c("A", "B"), each = 5)
-#' plot_heatmap(mat, feat = grp)
+#' plot_heatmap(mat, anno = grp)
 #'
 #' @export
 #' @importFrom purrr map_lgl
+#' @importFrom edgeR calcNormFactors cpm
+#' @importFrom DESeq2 sizeFactors normalizationFactors estimateSizeFactors
+#' @importFrom SummarizedExperiment assay
 #' @importFrom limma getEAWP
 #' @importFrom NMF aheatmap
 #' @import RColorBrewer
 #'
 
 plot_heatmap <- function(dat,
-                         feat = NULL,
+                         anno = NULL,
+                         dist = 'pearson',
+                    hclustfun = 'average',
                           col = 'RdBu',
                         title = NULL) {
 
   # Preliminaries
-  if (col == 'RdBu') {
-    cols <- colorRampPalette(brewer.pal(10L, 'RdBu'))(n = 256L)
-  } else if (col == 'GrRd') {
-    cols <- colorRampPalette(c('green', 'black', 'red'))(n = 256L)
+  if (is.data.frame(anno)) {
+    anno <- as.list(anno)
+  } else if (!is.list(anno)) {
+    anno <- list('Variable' = anno)
   } else {
-    stop('col must be one of "rb" or "gr".')
-  }
-  if (is.data.frame(feat)) {
-    feat <- as.list(feat)
-  } else if (!is.list(feat)) {
-    feat <- list('Variable' = feat)
-  } else {
-    if (is.null(names(feat))) {
-      if (length(feat) == 1L) {
-        names(feat) <- 'Variable'
+    if (is.null(names(anno))) {
+      if (length(anno) == 1L) {
+        names(anno) <- 'Variable'
       } else {
-        names(feat) <- paste('Variable', seq_along(feat))
+        names(anno) <- paste('Variable', seq_along(anno))
       }
     }
   }
-  if (any(map_lgl(seq_along(feat), function(j) {
-    length(feat[[j]]) != ncol(dat)
+  if (any(map_lgl(seq_along(anno), function(j) {
+    length(anno[[j]]) != ncol(dat)
   }))) {
-    stop('feat length must match number of samples in dat.')
+    stop('anno length must match number of samples in dat.')
   }
-  if (any(map_lgl(seq_along(feat), function(j) {
-    if (is.numeric(feat[[j]])) var(feat[[j]]) == 0L
-    else length(unique(feat[[j]])) == 1L
+  if (any(map_lgl(seq_along(anno), function(j) {
+    if (is.numeric(anno[[j]])) var(anno[[j]]) == 0L
+    else length(unique(anno[[j]])) == 1L
   }))) {
-    stop('feat is invariant.')
+    stop('anno is invariant.')
+  }
+  if (!dist %in% c('euclidean', 'pearson', 'MI', 'KLD')) {
+    stop('dist must be one of "euclidean", "pearson", "MI", or "KLD".')
+  }
+  if (!hclustfun %in% c('ward.D', 'ward.D2', 'single', 'complete', 'average',
+                        'mcquitty', 'median', 'centroid')) {
+    stop('hclustfun must be one of "ward.D", "ward.D2", "single", "complete", ',
+         '"average", "mcquitty", "median", or "centroid". See ?hclust.')
+  }
+  if (col == 'RdBu') {
+    col <- colorRampPalette(brewer.pal(10L, 'RdBu'))(n = 256L)
+  } else if (col == 'GrRd') {
+    col <- colorRampPalette(c('green', 'black', 'red'))(n = 256L)
+  } else if (col == 'BuYl') {
+    col <- colorRampPalette(c('blue', 'grey', 'yellow'))(n = 256L)
   }
   if (is.null(title)) {
     title <- 'Omic Heatmap'
   }
 
   # Tidy data
-  dat <- getEAWP(dat)$expr
-  keep <- rowSums(is.finite(dat)) == ncol(dat)
-  dat <- dat[keep, , drop = FALSE]
+  if (is(dat, 'DGEList')) {
+    keep <- rowSums(dat$counts) > 0L             # Minimal count filter
+    dat <- dat[keep, ]
+    if (is.null(dat$samples$norm.factors) |      # Calculate size factors
+        all(dat$samples$norm.factors == 1L)) {
+      dat <- calcNormFactors(dat)
+    }
+    dat <- cpm(dat, log = TRUE, prior.count = 1L)
+    warning('Transforming raw counts to log2-CPM scale.')
+  } else if (is(dat, 'DESeqDataSet')) {
+    if (is.null(sizeFactors(dat)) & is.null(normalizationFactors(dat))) {
+      dat <- estimateSizeFactors(dat)            # Normalize counts
+    }
+    dat <- counts(dat, normalized = TRUE)
+    keep <- rowMeans(dat) > 0L                   # Minimal count filter
+    dat <- dat[keep, , drop = FALSE]
+    dat <- cpm(dat, log = TRUE, prior.count = 1L)
+    warning('Transforming raw counts to log2-CPM scale.')
+  } else if (is(dat, 'DESeqTransform')) {
+    dat <- assay(dat)
+  } else {
+    dat <- getEAWP(dat)$expr
+    keep <- rowSums(is.finite(dat)) == ncol(dat)
+    dat <- dat[keep, , drop = FALSE]
+  }
 
   # Plot
-  if (is.null(feat)) {
-    aheatmap(dat, distfun = 'pearson', scale = 'row', col = cols,
-             hclustfun = 'average', main = title)
+  if (is.null(anno)) {
+    aheatmap(dat, distfun = dist, scale = 'row', col = col,
+             hclustfun = hclustfun, main = title)
   } else {
-    aheatmap(dat, distfun = 'pearson', scale = 'row', col = cols,
-             hclustfun = 'average', main = title, annCol = feat)
+    aheatmap(dat, distfun = dist, scale = 'row', col = col,
+             hclustfun = hclustfun, main = title, annCol = anno)
   }
 
 }
 
+
+# Replace aheatmap with pheatmap?
+# Rows should be centered, but not scaled, I think?
 
