3d38ff697904fb9df455e18b1201130703e769e6
dswatson <david.s.watson11@gmail.com>
2017-05-24 13:50:53 +0100

    quote fix

--- R/plot_tsne.R
+++ R/plot_tsne.R
@@ -71,7 +71,7 @@
 #' may be found in the references below.
 #'
 #' The \code{top} argument optionally filters data using either probewise
-#' variance (if \code{filter_method = "common"'}) or the leading fold change
+#' variance (if \code{filter_method = "common"}) or the leading fold change
 #' method of Smyth et al. (if \code{filter_method = "pairwise"}). See \code{
 #' \link{plot_mds}} for more details.
 #'
