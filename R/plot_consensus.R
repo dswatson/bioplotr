5f26a984c28ff48677b05849e38d245a4cacdf3d
dswatson <david.s.watson11@gmail.com>
2017-07-18 00:31:14 +0100

    color clusters

--- R/plot_consensus.R
+++ R/plot_consensus.R
@@ -12,13 +12,15 @@
 #'   Alternatively, a data frame or list of such vectors, optionally named.
 #' @param hclustfun The agglomeration method to be used for hierarchical
 #'   clustering. Supports any method available in \code{\link[stats]{hclust}}.
+#' @param pal_clust String specifying the color palette to use for cluster
+#'   assignments. Options include \code{"ggplot"}, all qualitative color schemes
+#'   available in \code{RColorBrewer}, and the complete collection of \code{
+#'   \href{http://bit.ly/2bxnuGB}{ggsci}} palettes.
 #' @param pal_group String specifying the color palette to use if \code{group}
 #'   is non-\code{NULL}, or a vector of such strings with length equal to the
-#'   number of vectors passed to \code{group}. Options include \code{"ggplot"},
-#'   all qualitative color schemes available in \code{RColorBrewer}, and the
-#'   complete collection of \code{\href{http://bit.ly/2bxnuGB}{ggsci}} palettes.
-#'   Alternatively, a character vector of colors with length equal to the
-#'   cumulative number of levels in \code{group}.
+#'   number of vectors passed to \code{group}. Options are the same as those for
+#'   \code{pal_clust}. Alternatively, a character vector of colors with length
+#'   equal to the cumulative number of levels in \code{group}.
 #' @param pal_covar String specifying the color palette to use if \code{covar}
 #'   is non-\code{NULL}, or a vector of such strings with length equal to the
 #'   number of vectors passed to \code{covar}. Options include all sequential
@@ -32,13 +34,13 @@
 #' @param title Optional plot title.
 #'
 #' @details
-#' The consensus cluster algorithm is a resampling procedure to evaluate cluster
-#' stability. A user-specified proportion of samples are held out on each run
-#' of the algorithm to test how often the remaining samples do or do not cluster
+#' Consensus clustering is a resampling procedure to evaluate cluster stability.
+#' A user-specified proportion of samples are held out on each run of the
+#' algorithm to test how often the remaining samples do or do not cluster
 #' together. The result is a square consensus matrix for each value of cluster
 #' numbers \emph{k}. Each cell of the matrix \code{mat[i, j]} represents the
-#' proportion of all runs for which samples \code{i} and \code{j} were clustered
-#' together.
+#' proportion of all runs including samples \code{i} and \code{j} in which the
+#' two were clustered together.
 #'
 #' \code{plot_consensus} converts a consensus matrix into a distance matrix by
 #' taking the complement of all values and visualising the result as a heatmap.
@@ -53,9 +55,20 @@
 #' 91-118.
 #'
 #' @examples
+#' # Load libraries
 #' library(ConsensusClusterPlus)
-#' data(airways)
 #'
+#' # Import, filter, transform data
+#' data(airway)
+#' mat <- assay(airway)
+#' mads <- apply(mat, 1, mad)
+#' keep <- order(mads, decreasing = TRUE)[seq_len(1000)]
+#' mat <- mat[keep, ]
+#' mat <- log2((mat + 1L) / 1e6L)
+#'
+#' # Run consensus clustering, plot results
+#' cc <- ConsensusClusterPlus(mat)
+#' plot_consensus(cc, k = 2)
 #'
 #' @export
 #' @importFrom purrr map_lgl
@@ -68,6 +81,7 @@ plot_consensus <- function(cc,
                            group = NULL,
                            covar = NULL,
                        hclustfun = 'average',
+                       pal_clust = 'd3',
                        pal_group = 'npg',
                        pal_covar = 'Blues',
                        pal_tiles = 'RdBu',
@@ -81,7 +95,18 @@ plot_consensus <- function(cc,
   if (k %>% is.null || !(k %>% is.numeric) || length(k) > 1L) {
     stop('k must be an integer on [2, length(cc)].')
   }
+  hclusts <- c('ward.D', 'ward.D2', 'single', 'complete', 'average',
+               'mcquitty', 'median', 'centroid')
+  if (!hclustfun %in% hclusts) {
+    stop('hclustfun must be one of ', stringify(hclusts, 'or'), '. ',
+         'See ?hclust.')
+  }
   dat <- 1L - cc[[k]]$consensusMatrix
+  clusters <- as.dist(dat) %>%
+    hclust(method = hclustfun) %>%
+    cutree(k = k)
+  clusters <- list('Cluster' = clusters)
+  clust_cols <- clusters %>% track_cols(pal_clust, var_type = 'Categorical')
   if (!(group %>% is.null)) {
     group <- dat %>% format_features(group, var_type = 'Categorical')
     grp_cols <- group %>% track_cols(pal_group, var_type = 'Categorical')
@@ -95,16 +120,11 @@ plot_consensus <- function(cc,
     cov_cols <- NULL
   }
   if (!(c(group, covar) %>% is.null)) {
-    anno <- c(group, covar)
-    ann_cols <- c(grp_cols, cov_cols)
+    anno <- c(clusters, group, covar)
+    ann_cols <- c(clust_cols, grp_cols, cov_cols)
   } else {
-    anno <- NULL
-  }
-  hclusts <- c('ward.D', 'ward.D2', 'single', 'complete', 'average',
-               'mcquitty', 'median', 'centroid')
-  if (!hclustfun %in% hclusts) {
-    stop('hclustfun must be one of ', stringify(hclusts, 'or'), '. ',
-         'See ?hclust.')
+    anno <- clusters
+    ann_cols <- clust_cols
   }
   pal_cols <- colorize(pal_tiles, var_type = 'Continuous')
   if (pal_tiles == 'RdBu') {
@@ -116,15 +136,9 @@ plot_consensus <- function(cc,
 
   # Build plot
   suppressPackageStartupMessages(require(NMF))
-  if (is.null(anno)) {
-    aheatmap(dat, col = pal_cols, Rowv = FALSE, revC = TRUE, main = title,
-             distfun = function(x) as.dist(x), hclustfun = hclustfun,
-             border_color = 'grey60')
-  } else {
-    aheatmap(dat, col = pal_cols, Rowv = FALSE, revC = TRUE, main = title,
-             distfun = function(x) as.dist(x), hclustfun = hclustfun,
-             annCol = anno, annColors = ann_cols, border_color = 'grey60')
-  }
+  aheatmap(dat, col = pal_cols, Rowv = FALSE, revC = TRUE, main = title,
+           distfun = function(x) as.dist(x), hclustfun = hclustfun,
+           annCol = anno, annColors = ann_cols, border_color = 'grey60')
 
 }
 
